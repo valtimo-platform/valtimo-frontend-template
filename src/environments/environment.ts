fb6a3c934a2340491147fb4ca869fa6e12bb812c
Ivo Zaal <ivo.zaal@ritense.com>
2025-06-18 12:13:36 +0200

    Fixed case management link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -22,7 +22,7 @@ export const environment: ValtimoConfig = {
       {
         roles: [ROLE_ADMIN], title: 'Admin', iconClass: 'icon mdi mdi-tune', sequence: 5, children: [
           {title: 'Configuration', textClass: 'text-dark font-weight-bold c-default', sequence: 1},
-          {link: ['/cases-management'], title: 'Cases', sequence: 2},
+          {link: ['/case-management'], title: 'Cases', sequence: 2},
           {link: ['/plugins'], title: 'Plugins', sequence: 3},
           {link: ['/dashboard-management'], title: 'Dashboard', sequence: 4},
           {link: ['/access-control'], title: 'Access Control', sequence: 5},
