25e304a9267b3fab2a90d4c43735b06580982db1
Ivo Zaal <ivo.zaal@ritense.com>
2025-07-18 10:41:43 +0200

    Upgrade Angular from 17 to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {Injector, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HttpBackend, HttpClientModule} from '@angular/common/http';
+import { HttpBackend, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LayoutModule, TranslationManagementModule} from '@valtimo/layout';
@@ -72,77 +72,71 @@ export function tabsFactory() {
   ]);
 }
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    HttpClientModule,
-    CommonModule,
-    BrowserModule,
-    AppRoutingModule,
-    LayoutModule,
-    CardModule,
-    WidgetModule,
-    BootstrapModule,
-    ConfigModule.forRoot(environment),
-    LoggerModule.forRoot(environment.logger),
-    environment.authentication.module,
-    SecurityModule,
-    MenuModule,
-    TaskModule,
-    DossierModule.forRoot(tabsFactory),
-    ProcessModule,
-    BpmnJsDiagramModule,
-    FormsModule,
-    ReactiveFormsModule,
-    DashboardModule,
-    DocumentModule,
-    AccountModule,
-    ChoiceFieldModule,
-    ResourceModule,
-    FormModule,
-    AnalyseModule,
-    SwaggerModule,
-    FormFlowManagementModule,
-    ProcessManagementModule,
-    DecisionModule,
-    MilestoneModule,
-    FormManagementModule,
-    ProcessLinkModule,
-    MigrationModule,
-    DossierManagementModule,
-    PluginManagementModule,
-    ObjectenApiPluginModule,
-    ObjecttypenApiPluginModule,
-    ObjectTokenAuthenticationPluginModule,
-    ObjectModule,
-    ObjectManagementModule,
-    AccessControlManagementModule,
-    DashboardManagementModule,
-    BigNumberModule,
-    CaseCountDataSourceModule,
-    AccessControlManagementModule,
-    HttpClientModule, TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: MultiTranslateHttpLoaderFactory,
-        deps: [HttpBackend, ConfigService]
-      }
-    }),
-    TranslationManagementModule,
-    LoggingModule
-  ],
-  providers: [{
-    provide: PLUGINS_TOKEN,
-    useValue: [
-      objectenApiPluginSpecification,
-      objecttypenApiPluginSpecification,
-      objectTokenAuthenticationPluginSpecification
-    ]
-  }],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [HttpClientModule,
+        CommonModule,
+        BrowserModule,
+        AppRoutingModule,
+        LayoutModule,
+        CardModule,
+        WidgetModule,
+        BootstrapModule,
+        ConfigModule.forRoot(environment),
+        LoggerModule.forRoot(environment.logger),
+        environment.authentication.module,
+        SecurityModule,
+        MenuModule,
+        TaskModule,
+        DossierModule.forRoot(tabsFactory),
+        ProcessModule,
+        BpmnJsDiagramModule,
+        FormsModule,
+        ReactiveFormsModule,
+        DashboardModule,
+        DocumentModule,
+        AccountModule,
+        ChoiceFieldModule,
+        ResourceModule,
+        FormModule,
+        AnalyseModule,
+        SwaggerModule,
+        FormFlowManagementModule,
+        ProcessManagementModule,
+        DecisionModule,
+        MilestoneModule,
+        FormManagementModule,
+        ProcessLinkModule,
+        MigrationModule,
+        DossierManagementModule,
+        PluginManagementModule,
+        ObjectenApiPluginModule,
+        ObjecttypenApiPluginModule,
+        ObjectTokenAuthenticationPluginModule,
+        ObjectModule,
+        ObjectManagementModule,
+        AccessControlManagementModule,
+        DashboardManagementModule,
+        BigNumberModule,
+        CaseCountDataSourceModule,
+        AccessControlManagementModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: MultiTranslateHttpLoaderFactory,
+                deps: [HttpBackend, ConfigService]
+            }
+        }),
+        TranslationManagementModule,
+        LoggingModule], providers: [{
+            provide: PLUGINS_TOKEN,
+            useValue: [
+                objectenApiPluginSpecification,
+                objecttypenApiPluginSpecification,
+                objectTokenAuthenticationPluginSpecification
+            ]
+        }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
   constructor(injector: Injector) {
     registerFormioUploadComponent(injector);
